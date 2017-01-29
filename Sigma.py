c4972620852a8eb5f309b6b3c288e6252a7ab911
Anurag Agrawal <aagraw25@asu.edu>
2017-01-28 19:44:39 -0700

    Update Sigma.py

--- Sigma.py
+++ Sigma.py
@@ -1,30 +1,44 @@
-import h5py
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 def dataReadPanda():
-    data = pd.read_hdf('C:/Users/Anurag/PycharmProjects/SigmaProject/train.h5/train.h5')
+    data = pd.read_hdf('./train.h5/train.h5')
     print data.head(5)
     return data
 
 def findOutTimeStamps(data):
-
     colmunDataFrame = data.groupby('id').timestamp.nunique()
     ids = set(data['id'])
     for id in ids:
         seven_data = data[data['id'] == id]
         seven_data[['timestamp', 'y']].sort_values(['timestamp']).plot(x='timestamp', y='y', kind='line')
-        saveStr = 'C:/Users/Anurag/PycharmProjects/SigmaProject/data_'.strip() + str(id).strip() + '.png'.strip()
+        saveStr = './data/data_'.strip() + str(id).strip() + '.png'.strip()
         print saveStr
         plt.savefig(saveStr)
         # plt.show()
-
     # print seven_data[['timestamp', 'y']].sort_values(['timestamp']).plot(x = 'timestamp', y = 'y', kind='line')
 
-def findCorrelation():
-    pass
+def get_mean(train):
+    means_ = train.mean(axis=0)
+    return means_
+
+def findOutColumnsCorrelation(data):
+    means_ = get_mean(data)
+    data.fillna(means_, inplace=True)
+    ids = set(data['id'])
+    name_of_col = data.columns
+    order_col_id = {}
+    for id in ids:
+        corDict = {}
+        seven_data = data[data['id'] == id]
+        y_data = seven_data['y']
+        for c in name_of_col:
+            corDict[c] = np.corrcoef(y_data, np.array(seven_data[c]))[0, 1]
+        corD = sorted(corDict, key=corDict.get, reverse=True)
+        order_col_id[id] = corD
+    return order_col_id
 
 if __name__ == "__main__":
     data = dataReadPanda()
-    findOutTimeStamps(data)
+    # findOutTimeStamps(data)
+    findOutColumnsCorrelation(data)
